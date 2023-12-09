8e96f9c32bbb0fb2ebc970f796c788aea69a830a
trent.wyatt <trent@trentwyatt.com>
2023-12-08 20:45:14 -0600

    Update macSpeechSynthesizer.ino

--- PublicGallery/macSpeechSynthesizer/macSpeechSynthesizer.ino
+++ PublicGallery/macSpeechSynthesizer/macSpeechSynthesizer.ino
@@ -16,7 +16,6 @@
 SoftwareSerial command_serial(RX_PIN, TX_PIN);  // RX, TX
 
 void setup() {
-    Serial.begin(115200);
     command_serial.begin(9600);
 
     // the text you want your Mac to speak
