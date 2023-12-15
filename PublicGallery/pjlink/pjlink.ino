e8378beb3815cf1beedd7e831fb330d6ed0768db
trent.wyatt <trent@trentwyatt.com>
2023-12-15 03:33:24 -0600

    Create pjlink.ino
    Added an example sketch showing how to make use of the Python pjlink library to control projectors and other devices that use the PJLINK Protocol.
    
    Requires installation of the pjlink Python library:
    `pip install pjlink`

---
+++ PublicGallery/pjlink/pjlink.ino
@@ -0,0 +1,37 @@
+/*
+ * pjlink.ino
+ *
+ * arduinoCLI sketch to retrieve information about
+ * any pjlink device to your Arduino and allow you to control them!
+ */
+
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+#define  RX_PIN     7
+#define  TX_PIN     8
+
+// Software Serial object to send the
+// commands to the Python Agent
+SoftwareSerial command_serial(RX_PIN, TX_PIN);  // RX, TX
+
+void setup() {
+    Serial.begin(115200);
+    command_serial.begin(9600);
+
+    String command = "pjlink --help";
+    command_serial.println(command);
+
+    uint32_t start = millis();
+    while (millis() - start < 2000) {
+        while (command_serial.available() > 0) {
+            String output = command_serial.readString();
+            output.trim();
+            if (output.length() != 0) {
+                Serial.println(output);
+            }
+        }
+    }
+}
+
+void loop() { }
