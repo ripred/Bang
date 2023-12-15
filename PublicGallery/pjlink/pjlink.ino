4491a7d4ef07d70cb089cc1b57279410e64ca0f9
trent.wyatt <trent@trentwyatt.com>
2023-12-15 03:54:07 -0600

    Added instructions and notes to the sketch comments

--- PublicGallery/pjlink/pjlink.ino
+++ PublicGallery/pjlink/pjlink.ino
@@ -3,6 +3,15 @@
  *
  * arduinoCLI sketch to retrieve information about
  * any pjlink device to your Arduino and allow you to control them!
+ * 
+ * NOTE:
+ * In order to use the pjlink command line tools you will need to install the Python pjlink library:
+ * 
+ * $ pip install pjlink
+ * 
+ * Full instructions for the use of the pjlink configuration and cli are available here:
+ * https://blog.flowblok.id.au/2012-11/controlling-projectors-with-pjlink.html
+ * 
  */
 
 #include <SoftwareSerial.h>
