f8d5c32d957df5320bf44fa4939f5a585c518e92
trent.wyatt <trent@trentwyatt.com>
2024-01-04 12:18:04 -0600

    Create ArduinoCLI.ino

---
+++ ArduinoCLI.ino
@@ -0,0 +1,41 @@
+/*
+ * ArduinoCLI.ino
+ * 
+ * Example use of using your PC/Mac/Linux machine
+ * as a service for your Arduino projects.
+ * 
+ */
+
+#include <SoftwareSerial.h>
+
+#define  RX_PIN   7
+#define  TX_PIN   8
+
+SoftwareSerial command_serial(RX_PIN, TX_PIN);  // RX, TX
+
+void setup() {
+    Serial.begin(115200);
+    command_serial.begin(38400);
+    command_serial.println("!echo \"Hello Arduino!\"");
+}
+
+void loop() {
+    if (Serial.available()) {
+        String cmd = Serial.readString();
+        cmd.trim();
+        if (cmd.length()) {
+            Serial.print(F("User command \""));
+            Serial.print(cmd);
+            Serial.println(F("\" received"));
+            command_serial.println(cmd);
+        }
+    }
+
+   while (command_serial.available() > 0) {
+        String output = command_serial.readString();
+        output.trim();
+        if (output.length() != 0) {
+            Serial.println(output);
+        }
+    }
+}
