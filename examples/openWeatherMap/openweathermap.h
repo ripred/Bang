3940bf1886cd2d9aeca7295518edc6463fed540b
trent.wyatt <trent@trentwyatt.com>
2024-01-12 03:44:39 -0600

    Update openweathermap.h

--- examples/openWeatherMap/openweathermap.h
+++ examples/openWeatherMap/openweathermap.h
@@ -2,7 +2,7 @@
 #define OPENWEATHER_MAP_H_INCL
 
 // create a free account at openweathermap.org and copy your key
-#define  API_KEY    "3333404d9bb27741f4a043c0c4c2f36e"
-#define  ZIPCODE    "75002"
+#define  API_KEY    "*****"
+#define  ZIPCODE    "*****"
 
 #endif
