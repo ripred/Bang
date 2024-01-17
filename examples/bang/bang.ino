289b931346fd9b370092d24a4d130732ed13af41
trent.wyatt <trent@trentwyatt.com>
2024-01-17 08:34:01 -0600

    updated to use the new Bang object and api

--- examples/bang/bang.ino
+++ examples/bang/bang.ino
@@ -7,16 +7,32 @@
  */
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include <Bang.h>
 
-#define  RX_PIN   7
-#define  TX_PIN   8
+// Instructions For Use:
+// Define the manifest constant USE_FTDI if you want to use a separate FTDI module to issue commands on
+//   which frees the Serial output for normal debugging output
+// 
+// You can use just the Serial port by itself to issue commands and retreive the output
+// but then you cannot use the Serial Monitor window for debugging.
 
-SoftwareSerial command_serial(RX_PIN, TX_PIN);  // RX, TX
+// Un-comment to use an FTDI port for commands and the Serial port for debugging output
+#define  USE_FTDI
 
-// class wrapper for the Bang api so far:
+#ifdef  USE_FTDI
+#include <SoftwareSerial.h>
+enum { RX_PIN = 7, TX_PIN = 8 };
+SoftwareSerial command_serial(RX_PIN, TX_PIN);
+
+// class wrapper for the Bang api so far using two serial ports:
 Bang bang(command_serial, Serial);
+
+#else
+
+// class wrapper for the Bang api so far using one serial port:
+Bang bang(Serial);
+
+#endif
 
 void setup() {
     Serial.begin(115200);
