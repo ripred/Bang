c27f9f3898605938ee74c5aaf725177b9debb2ec
trent.wyatt <trent@trentwyatt.com>
2024-01-17 08:34:16 -0600

    updated to use the new Bang object and api

--- examples/hue/hue.ino
+++ examples/hue/hue.ino
@@ -5,12 +5,35 @@
  * 
  */
 
+#include <Arduino.h>
+#include <Bang.h>
+
+// Instructions For Use:
+// Define the manifest constant USE_FTDI if you want to use a separate FTDI module to issue commands on
+//   which frees the Serial output for normal debugging output
+// 
+// You can use just the Serial port by itself to issue commands and retreive the output
+// but then you cannot use the Serial Monitor window for debugging.
+
+// Un-comment to use an FTDI port for commands and the Serial port for debugging output
+//#define  USE_FTDI
+
+#ifdef  USE_FTDI
 #include <SoftwareSerial.h>
+enum { RX_PIN = 7, TX_PIN = 8 };
+SoftwareSerial command_serial(RX_PIN, TX_PIN);
 
-#define RX_PIN 7
-#define TX_PIN 8
-#define HUE_BRIDGE_IP "192.168.1.2"  // Replace with your Hue Bridge IP
+// class wrapper for the Bang api so far using two serial ports:
+Bang bang(command_serial, Serial);
 
+#else
+
+// class wrapper for the Bang api so far using one serial port:
+Bang bang(Serial);
+
+#endif
+
+#define HUE_BRIDGE_IP "192.168.1.2"    // Replace with your Hue Bridge IP
 #define YOUR_USERNAME "your_username"  // Replace with your Hue Bridge username
 
 // change to match your Hue Bridge group numbers
@@ -18,33 +41,31 @@
 #define LIGHT_ID_2 2
 #define GROUP_ID 1
 
-SoftwareSerial command_serial(RX_PIN, TX_PIN);  // RX, TX
-
 // Function to create and format the command string
 String formatCommand(String apiEndpoint, String method, String additionalParams = "") {
-    return "!curl -s -X " + method + " -H \"Content-Type: application/json\" -d '" + additionalParams +
+    return "curl -s -X " + method + " -H \"Content-Type: application/json\" -d '" + additionalParams +
            "' http://" + HUE_BRIDGE_IP + "/api/" + YOUR_USERNAME + "/" + apiEndpoint;
 }
 
 // Function to control the state of a light
 void controlLightState(int lightId, String command) {
     String apiEndpoint = "lights/" + String(lightId) + "/state";
-    command_serial.println(formatCommand(apiEndpoint, "PUT", command));
+    bang.exec(formatCommand(apiEndpoint, "PUT", command).c_str());
 }
 
 // Function to control the state of a group
 void controlGroupState(int groupId, String command) {
     String apiEndpoint = "groups/" + String(groupId) + "/action";
-    command_serial.println(formatCommand(apiEndpoint, "PUT", command));
+    bang.exec(formatCommand(apiEndpoint, "PUT", command).c_str());
 }
 
 // Function to get information about the Hue Bridge
 void getBridgeInfo() {
-    command_serial.println(formatCommand("", "GET"));
+    bang.exec(formatCommand("", "GET").c_str());
 }
 
 void setup() {
-    command_serial.begin(9600);
+    Serial.begin(38400);
 
     // Turn on light 1
     controlLightState(LIGHT_ID_1, "{\"on\":true}");
